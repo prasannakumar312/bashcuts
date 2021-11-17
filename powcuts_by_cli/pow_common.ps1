c1277fef9d0956269d16896652f3fd4168aa8444
dtc-release-data-operations@va.gov <dtc-release-data-operations@va.gov>
2021-11-17 16:24:18 -0500

    bashcut updates -- time stamps for dx

--- powcuts_by_cli/pow_common.ps1
+++ powcuts_by_cli/pow_common.ps1
@@ -10,3 +10,8 @@ function reinit {
 function new-list($new_list_variable) {
     Set-Variable -Name "$new_list_variable" -Value ([system.collections.generic.list[string]]::new()) -Scope global
 }
+
+function robot-debug() {
+    $env:ROBOT_DEBUG = "TRUE"; robot --rpa -d output .
+}
+
--- powcuts_by_cli/pow_open.ps1
+++ powcuts_by_cli/pow_open.ps1
@@ -1,19 +1,19 @@
 
 
 
-function open-profile {
+function o-profile {
 	code $profile
 } 	
-Set-Alias -Name o-profile -Value open-profile
+Set-Alias -Name o-profile -Value o-profile
 
 
-function open-pow-common{
+function o-pow-common{
 	code 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pow_common.ps1'
 }
-Set-Alias -Name o-pow-common -Value open-pow-common
+Set-Alias -Name o-pow-common -Value o-pow-common
 
 
-function open-pester{
+function o-pester{
 	code 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pester.ps1'
 }
-Set-Alias -Name o-pester -Value open-pester
+Set-Alias -Name o-pester -Value o-pester
