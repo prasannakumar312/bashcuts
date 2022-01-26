66673b951c03bc10b85002ce6fdcc8ff926379e7
jdschleicher <jdschleicher@gmail.com>
2022-01-26 15:02:14 -0500

    updates

--- powcuts_by_cli/pow_common.ps1
+++ powcuts_by_cli/pow_common.ps1
@@ -3,10 +3,10 @@
 
 
 Set-Alias -Name nfp -Value c:\windows\notepad.exe
-function reinit {
-    Invoke-Command { & "powershell.exe" } -NoNewScope # PowerShell 5
-}
 
+function reinit {
+    Invoke-Command { & "pwsh.exe" } -NoNewScope 
+}
 function new-list($new_list_variable) {
     Set-Variable -Name "$new_list_variable" -Value ([system.collections.generic.list[string]]::new()) -Scope global
 }
