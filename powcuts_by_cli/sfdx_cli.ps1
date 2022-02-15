7d9d68921d9382b082a39967bf8478ed4561fa78
jdschleicher <jdschleicher@gmail.com>
2022-02-15 05:14:00 -0500

    updates

--- powcuts_by_cli/sfdx_cli.ps1
+++ powcuts_by_cli/sfdx_cli.ps1
@@ -15,4 +15,40 @@ function pernit {
 	SetupLocalGitHubActionsEnvironment
 }
 
+function is_workflow_alert_with_orrgWideEmailAddress {
+    param($alert)
+
+    [boolean]$isWorkflowAlertWithOrgWideEmailAddress = $(
+                                                    $alert -ne $null                            -and
+                                                    $alert.senderType -ne $null                -and 
+                                                    $alert.senderType -eq 'OrgWideEmailAddress' 
+                                                )
+    $isWorkflowAlertWithOrgWideEmailAddress
+}
+
+function dx-temporary_replacement_orgwideemail_replace_for_scratch_org_push {
+    
+    $all_workflow_files = Get-ChildItem -Recurse -File *.workflow-meta.xml
+
+    foreach ($file in $all_workflow_files) {
+        $xml_file = [xml](Get-Content $file -Raw)
+
+        if ($xml_file.Workflow.alerts -ne $null) {
+            (0..($xml_file.Workflow.alerts.count - 1)) | foreach {
+
+                if (is_workflow_alert_with_orrgWideEmailAddress -alert $xml_file.Workflow.alerts[$_]) {
+                    $xml_file.Workflow.alerts[$_].senderType = 'CurrentUser'
+                    $sender_address_xml_node = $xml_file.Workflow.alerts[$_].ChildNodes | where Name -eq 'senderAddress'
+                    $sender_address_xml_node.ParentNode.RemoveChild($sender_address_xml_node) | Out-Null  
+                }
+       
+            }
+
+            $xml_file.Save($file)
+        }
+    }
+
+}
+
+
 
