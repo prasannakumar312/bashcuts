fa95476d649be98564e89e2ba994e277765e068b
jdschleicher <jdschleicher@gmail.com>
2022-02-18 14:22:11 -0500

    removing hard coded "duration"

--- powcuts_by_cli/sfdx_cli.ps1
+++ powcuts_by_cli/sfdx_cli.ps1
@@ -26,6 +26,17 @@ function is_workflow_alert_with_orrgWideEmailAddress {
     $isWorkflowAlertWithOrgWideEmailAddress
 }
 
+function is_workflow_alert_with_currentUser {
+    param($alert)
+
+    [boolean]$is_workflow_alert_with_currentUser = $(
+                                                    $alert -ne $null                            -and
+                                                    $alert.senderType -ne $null                -and 
+                                                    $alert.senderType -eq 'CurrentUser' 
+                                                )
+    $is_workflow_alert_with_currentUser
+}
+
 function dx-temporary_replacement_orgwideemail_replace_for_scratch_org_push {
     
     $all_workflow_files = Get-ChildItem -Recurse -File *.workflow-meta.xml
@@ -38,6 +49,30 @@ function dx-temporary_replacement_orgwideemail_replace_for_scratch_org_push {
 
                 if (is_workflow_alert_with_orrgWideEmailAddress -alert $xml_file.Workflow.alerts[$_]) {
                     $xml_file.Workflow.alerts[$_].senderType = 'CurrentUser'
+                    $sender_address_xml_node = $xml_file.Workflow.alerts[$_].ChildNodes | where Name -eq 'senderAddress'
+                    $sender_address_xml_node.ParentNode.RemoveChild($sender_address_xml_node) | Out-Null  
+                }
+       
+            }
+
+            $xml_file.Save($file)
+        }
+    }
+
+}
+
+function dx-undo-temporary_replacement_orgwideemail_replace_for_scratch_org_push {
+    
+    $all_workflow_files = Get-ChildItem -Recurse -File *.workflow-meta.xml
+
+    foreach ($file in $all_workflow_files) {
+        $xml_file = [xml](Get-Content $file -Raw)
+
+        if ($xml_file.Workflow.alerts -ne $null) {
+            (0..($xml_file.Workflow.alerts.count - 1)) | foreach {
+
+                if (is_workflow_alert_with_currentUser -alert $xml_file.Workflow.alerts[$_]) {
+                    $xml_file.Workflow.alerts[$_].senderType = 'OrgWideEmailAddress'
                     $sender_address_xml_node = $xml_file.Workflow.alerts[$_].ChildNodes | where Name -eq 'senderAddress'
                     $sender_address_xml_node.ParentNode.RemoveChild($sender_address_xml_node) | Out-Null  
                 }
