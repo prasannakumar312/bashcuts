67459d8f86d7db9dd4774bef71c66af593e85a6b
jdschleicher <jdschleicher@gmail.com>
2022-04-27 15:26:10 -0400

    rmeoving hard coded paths for variables

--- powcuts_by_cli/pow_open.ps1
+++ powcuts_by_cli/pow_open.ps1
@@ -2,18 +2,27 @@
 
 
 function o-profile {
-	code $profile
+	code $profile #Microsoft.Powershell_profile.ps1
 } 	
-Set-Alias -Name o-profile -Value o-profile
 
-
-function o-pow-common{
-	code 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pow_common.ps1'
+function o-pow-common {
+	code "$path_to_bashcuts\powcuts_by_cli\pow_common.ps1"
 }
-Set-Alias -Name o-pow-common -Value o-pow-common
 
-
-function o-pester{
-	code 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pester.ps1'
+function o-pester {
+	code "$path_to_bashcuts\powcuts_by_cli\pester.ps1"
 }
-Set-Alias -Name o-pester -Value o-pester
+
+function o-sfdx-pwsh {
+	code "$path_to_bashcuts\powcuts_by_cli\sfdx_cli.ps1"
+}
+
+Set-Alias -Name nfp -Value c:\windows\notepad.exe
+
+function reinit {
+    Invoke-Command { & "pwsh.exe" } -NoNewScope 
+}
+
+function new-list($new_list_variable) {
+    Set-Variable -Name "$new_list_variable" -Value ([system.collections.generic.list[string]]::new()) -Scope global
+}
--- powcuts_home.ps1
+++ powcuts_home.ps1
@@ -1,30 +1,30 @@
   Write-Host "powershell starting"
 
-$pow_sfdxcli = Get-Content 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\sfdx_cli.ps1'
+$pow_sfdxcli = Get-Content "$path_to_bashcuts\powcuts_by_cli\sfdx_cli.ps1"
 if ($pow_sfdxcli -ne $NULL) {
- . 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\sfdx_cli.ps1'
+ . "$path_to_bashcuts\powcuts_by_cli\sfdx_cli.ps1"
 } else {
     Write-Host "no sfdx cli"
 }
 
 
-$pow_common = Get-Content 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pow_common.ps1'
+$pow_common = Get-Content "$path_to_bashcuts\powcuts_by_cli\pow_common.ps1"
 if ($pow_common -ne $NULL) {
- . 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pow_common.ps1'
+ . "$path_to_bashcuts\powcuts_by_cli\pow_common.ps1"
 } else {
     Write-Host "no powcli"
 }
 
-$pow_open = Get-Content 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pow_open.ps1'
+$pow_open = Get-Content "$path_to_bashcuts\powcuts_by_cli\pow_open.ps1"
 if ($pow_open -ne $NULL) {
- . 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pow_open.ps1'
+ . "$path_to_bashcuts\powcuts_by_cli\pow_open.ps1"
 } else {
     Write-Host "no pow_open"
 }
 
-$pester = Get-Content 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pester.ps1'
+$pester = Get-Content "$path_to_bashcuts\powcuts_by_cli\pester.ps1"
 if ($pow_open -ne $NULL) {
- . 'C:\git\jdschleicher\bashcuts\powcuts_by_cli\pester.ps1'
+ . "$path_to_bashcuts\powcuts_by_cli\pester.ps1"
 } else {
     Write-Host "no pester"
 }
